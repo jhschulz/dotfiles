ea45e1dff80fe3155290f572695430f503fdea5d
Jack Schulz <jhschulz@yahoo.com>
2025-08-17 20:07:28 -0500

    Added keybind for diagnostic float

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,4 +1,5 @@
 require('which-key').add({
     { '<leader>x', group = 'Diagnostics' },
     { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics Window' },
+    { '<leader>xf', vim.diagnostic.open_float, desc = 'Diagnostics Float' },
 })
